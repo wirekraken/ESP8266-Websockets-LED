f6897fa67c6e7147af1ee4402196501ccf179960
wkraken <wirekraken@gmail.com>
2023-12-09 12:03:26 +0200

    frontend: update ws

--- ESP8266-LED/data/ws.js
+++ ESP8266-LED/data/ws.js
@@ -3,26 +3,26 @@ import { UI } from './init.js';
 let ws;
 
 function wsConnect() {
-	UI.error.ws.style.display = 'flex';
-	
-	ws = new WebSocket('ws://' + window.location.hostname + ':8080');
-	ws.onopen = onOpen;
-	ws.onclose = onClose;
-	ws.onerror = onError;
+  UI.error.ws.style.display = 'flex';
+  
+  ws = new WebSocket('ws://' + window.location.hostname + ':8080');
+  ws.onopen = onOpen;
+  ws.onclose = onClose;
+  ws.onerror = onError;
 
 }
 
 function onClose(e) {
-	setTimeout(wsConnect, 1000);
+  setTimeout(wsConnect, 1000);
 }
 
 function onError(e) {
-	console.log('Error');
-	UI.error.ws.style.display = 'flex';
+  console.log(`[Error] ${e}`);
+  UI.error.ws.style.display = 'flex';
 }
 
 function onOpen() {
-	UI.error.ws.style.display = 'none';
+  UI.error.ws.style.display = 'none';
 }
 
 export { wsConnect, ws };
